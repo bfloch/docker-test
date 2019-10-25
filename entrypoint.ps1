5fd7faffea5cc6d3f12ffcdbdc94f4b345c67cd6
Blazej Floch <register@bfloch.com>
2019-10-25 11:50:58 -0400

    Update entrypoint.ps1

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -1,5 +1,28 @@
-Write-Host "Num Args:" $args.Length;
-foreach ($arg in $args) {
-    echo "Hello: $arg";
-}
-Get-Date
+param( $repo, $commit )
+
+echo "Testing $repo at $commit"
+
+# Verify cmake
+cmake.exe --version
+
+#Verify pwsh
+pwsh --version
+
+#Verify git
+git --version
+
+#Verify python
+python --version
+
+
+# Clone repo
+git clone https://github.com/$repo rez
+cd rez
+git checkout $commit
+
+# Install rez
+mkdir build
+python install.py build
+
+#Run Rez Tests
+.\build\Scripts\rez\rez-selftest.exe
